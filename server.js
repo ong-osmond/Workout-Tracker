0b2234ea628920994ec687d97f1d7ff10c202fe3
ong_osmond <ong.osmond@yahoo.com>
2020-09-02 08:41:53 +0800

    Deploying to Heroku

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 
 // Route to new Exercise page
